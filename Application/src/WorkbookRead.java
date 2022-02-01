7eed13ecfb6d9ab6810f61bae52f7814b33964bb
Ali Fahad <58633887+limitless-af10@users.noreply.github.com>
2022-02-01 08:34:53 +0500

    formatted code

--- Application/src/WorkbookRead.java
+++ Application/src/WorkbookRead.java
@@ -7,27 +7,26 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 
-
 public class WorkbookRead {
-	
+
 	private FileInputStream inputStream;
-	private Workbook workbook; 
-	
-	public WorkbookRead(String filePath) throws IOException{
+	private Workbook workbook;
+
+	public WorkbookRead(String filePath) throws IOException {
 		inputStream = new FileInputStream(new File(filePath));
 		workbook = new XSSFWorkbook(inputStream);
-		
+
 	}
-	
+
 	public Workbook getWorkbook() {
 		return this.workbook;
 	}
-	
-	public void close() throws IOException{
+
+	public void close() throws IOException {
 		workbook.close();
-        inputStream.close();
+		inputStream.close();
 	}
-	
+
 	public Sheet getSheet(int sheet_number) {
 		return this.workbook.getSheetAt(sheet_number);
 	}
--- Application/src/WorkbookWrite.java
+++ Application/src/WorkbookWrite.java
@@ -6,26 +6,25 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class WorkbookWrite {
 
 	private XSSFWorkbook workbook;
-	
-	public WorkbookWrite(){
-		
+
+	public WorkbookWrite() {
+
 		workbook = new XSSFWorkbook();
 	}
-	
+
 	public XSSFWorkbook getWorkbook() {
 		return this.workbook;
 	}
-	
+
 	public boolean writeToFile(String fileName) {
 		try {
-            FileOutputStream out = new FileOutputStream(new File(fileName));
-            this.workbook.write(out);
-            out.close();
-            return true;
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+			FileOutputStream out = new FileOutputStream(new File(fileName));
+			this.workbook.write(out);
+			out.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
